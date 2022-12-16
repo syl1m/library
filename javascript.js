0d37716f481267763332775fccc115e325177567
syl1m <shengyaulim@gmail.com>
2022-12-16 16:38:09 -0600

    Add HTML table and function to list books in table

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
+const libraryTableBody = document.querySelector('tbody');
 let myLibrary = [];
+
+// Placeholder books
+const book1 = new Book("The Way of Kings", "Brandon Sanderson", "500", "read");
+const book2 = new Book("Dune", "Frank Herbert", "354", "read");
+const book3 = new Book("Eragon", "Christopher Paolini", "1000", "not read");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+// 
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -11,12 +22,16 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-// Placeholder books
-const book1 = new Book("The Way of Kings", "Brandon Sanderson", "500", "read");
-addBookToLibrary(book1);
+function displayBook() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const bookRow = document.createElement('tr');
+        for(let prop in myLibrary[i]) {
+            const bookInfoCell = document.createElement('td');
+            bookInfoCell.textContent = `${myLibrary[i][prop]}`;
+            bookRow.appendChild(bookInfoCell);
+        }
+        libraryTableBody.appendChild(bookRow);
+    }
+}
 
-const book2 = new Book("Dune", "Frank Herbert", "354", "read");
-addBookToLibrary(book2);
-
-const book3 = new Book("Eragon", "Christopher Paolini", "1000", "not read");
-addBookToLibrary(book3);
+displayBook()
