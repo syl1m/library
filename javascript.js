6fb03b66d0e573e7a9b33b0b3fa9bbf63108a13b
syl1m <shengyaulim@gmail.com>
2022-12-16 23:43:34 -0600

    Add CSS styling for layout and colors

--- javascript.js
+++ javascript.js
@@ -5,10 +5,14 @@ let myLibrary = [];
 const book1 = new Book("The Way of Kings", "Brandon Sanderson", "500", "read");
 const book2 = new Book("Dune", "Frank Herbert", "354", "read");
 const book3 = new Book("Eragon", "Christopher Paolini", "1000", "not read");
+const book4 = new Book("The Lightning Thief", "Rick Riordan", "400", "read");
+const book5 = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", "423", "not read");
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
 // 
 
 function Book(title, author, pages, read) {
